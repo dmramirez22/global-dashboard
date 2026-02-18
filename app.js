9eb4e4b0b09fcdf28f856d61f9e820cc7b3bf318
Danna <dannaa2201@gmail.com>
2026-02-18 10:08:41 -0500

    chart js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 async function getGDP(country="USA") {
-    const res = await fetch (`https://api.worldbank.org/v2/country/USA/indicator/NY.GDP.MKTP.CD?format=json`)
+    const res = await fetch (`https://api.worldbank.org/v2/${country}/USA/indicator/NY.GDP.MKTP.CD?format=json`)
     const data = await res.json();
     console.log(data)
 }
+
+
+
